efa405918830e335528db09c31bbe1ce934ec304
Ryan <ryan1029@live.cn>
2018-10-07 23:20:47 +0800

    extension

---
+++ DotNetCore_Library/ExtensionMethod/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethod
+{
+    public static class ExtensionMethod
+    {
+        /// <summary>
+        /// replace below code by using this extension method
+        /// if(reallyLongIntegerVariableName == 1 || reallyLongIntegerVariableName == 6 || 
+        /// reallyLongIntegerVariableName == 9 || reallyLongIntegerVariableName == 11){
+        /// do something....}
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static bool In<T>(this T source, params T[] list)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+            return list.Contains(source);
+        }
+
+        
+    }
+}
